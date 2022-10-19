4baa8505eb712af4038c91fad48ebe4e600164db
RejonSJ <105995583+RejonSJ@users.noreply.github.com>
2022-10-19 11:16:37 -0500

    Add files via upload

---
+++ WebApi/Models/User.cs
@@ -0,0 +1,18 @@
+using WebApi.Models.Base;
+
+namespace WebApi.Models;
+
+public class User: Model
+{
+    public string? Name {get; set;}
+
+    public string? LastName {get; set;}
+    
+    public int? TelephoneNumber {get; set;}
+
+    public string? Address {get; set;}
+
+    public virtual IList<Borrow>? Borrows { get; set; } = default!;
+    
+    
+}
